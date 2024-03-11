13c036de2883e8207a999e4ff3db651b6a69c09d
Daniel Gwon <dygwon@gmail.com>
2024-03-10 20:39:27 -0400

    minor refactoring

--- external/runifeval.py
+++ external/runifeval.py
@@ -6,8 +6,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+# make sure to update these paths if the relative path
+# to instruction_following_eval/ changes
+DATA_PATH = os.path.join('.', 'instruction_following_eval', 'data')
 PROMPTS_PATH = os.path.join(
-    '.', 'instruction_following_eval', 'data', 'input_response_data_gpt4_20231107_145030.jsonl')
+    DATA_PATH, 'input_response_data_gpt4_20231107_145030.jsonl')
 
 
 def get_llm_response(messages: list, model: str) -> str:
@@ -68,15 +71,15 @@ def get_ifeval_input_data(model: str, prompts: list) -> list:
     return prompt_response_list
 
 
-def save_ifeval_input_data(model: str, prompt_responses: list, model_datetime: str) -> str:
+def save_ifeval_input_data(
+        model: str, prompt_responses: list, model_datetime: str) -> str:
     # saved files are differentiated by model and date
     model_name = model.split('/')[-1]
     filename = '_'.join([
         'input_response_data',
         model_name,
         model_datetime]) + '.jsonl'
-    save_filepath = os.path.join(
-        '.', 'instruction_following_eval', 'data', filename)
+    save_filepath = os.path.join(DATA_PATH, filename)
 
     with open(save_filepath, mode='w', encoding='utf-8') as save_file:
         for prompt_response in prompt_responses:
@@ -95,7 +98,7 @@ def save_ifeval_input_data(model: str, prompt_responses: list, model_datetime: s
 
 
 def get_instruction_ids() -> list:
-    input_data_file = os.path.join('.', 'instruction_following_eval', 'data', 'input_data.jsonl')
+    input_data_file = os.path.join(DATA_PATH, 'input_data.jsonl')
 
     instruction_ids = set()
     with open(input_data_file, mode='r', encoding='utf-8') as f:
@@ -109,10 +112,9 @@ def get_instruction_ids() -> list:
 
 
 def save_evaluation_results(model: str, model_datetime: str):
-    data_path = os.path.join('.', 'instruction_following_eval', 'data')
-    loose_file = os.path.join(data_path, 'eval_results_loose.jsonl')
-    strict_file = os.path.join(data_path, 'eval_results_strict.jsonl')
-    eval_results_file = os.path.join(data_path, 'eval_results.csv')
+    loose_file = os.path.join(DATA_PATH, 'eval_results_loose.jsonl')
+    strict_file = os.path.join(DATA_PATH, 'eval_results_strict.jsonl')
+    eval_results_file = os.path.join(DATA_PATH, 'eval_results.csv')
     all_instruction_ids = get_instruction_ids()
 
     # pull all true from evaluation categories
@@ -135,23 +137,30 @@ def save_evaluation_results(model: str, model_datetime: str):
                 # record results at the instruction level
                 inst_results = jline["follow_instruction_list"]
                 line_instruction_ids = jline["instruction_id_list"]
-                for instruction_id, result in zip(line_instruction_ids, inst_results):
+                for instruction_id, result in zip(
+                    line_instruction_ids, inst_results):
                     # print(instruction_id, result)
                     if result:
                         eval_dict[instruction_id] += 1
 
         # store the results for this model and accuracy in a csv file
         need_header = False if os.path.exists(eval_results_file) else True
-        with open(eval_results_file, mode='a', encoding='utf-8', newline='') as csvfile:
+        with open(eval_results_file, mode='a',
+                  encoding='utf-8', newline='') as csvfile:
             eval_results_writer = csv.writer(csvfile)
 
             if need_header:
-                header_row = ['model', 'datetime', 'accuracy', 'follow_all_instructions'] + all_instruction_ids
+                header_row = ['model',
+                              'datetime',
+                              'accuracy',
+                              'follow_all_instructions'] + all_instruction_ids
                 eval_results_writer.writerow(header_row)
 
             # records the results
-            results_row = [eval_dict["model"], eval_dict["datetime"],
-                           eval_dict["accuracy"], eval_dict["follow_all_instructions"]]
+            results_row = [eval_dict["model"],
+                           eval_dict["datetime"],
+                           eval_dict["accuracy"],
+                           eval_dict["follow_all_instructions"]]
             # record the results for each instruction id
             for instruction_id in all_instruction_ids:
                 results_row.append(eval_dict[instruction_id])
@@ -170,7 +179,7 @@ def main():
     for model in models:
         for runidx in range(3):
             print(f"{model} run {runidx + 1}")
-            
+
             model_datetime = datetime.datetime.today().strftime('%F_%T')
 
             input_response_data = get_ifeval_input_data(model, prompts)
