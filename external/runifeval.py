be2e84bcfb4460569e1d10da5eb80a4ff6527f01
Daniel Gwon <55669020+dygwon@users.noreply.github.com>
2024-03-26 23:00:37 -0400

    Improve error handling for faulty responses

--- external/runifeval.py
+++ external/runifeval.py
@@ -1,4 +1,4 @@
-import os, json, pprint, datetime, csv, subprocess
+import os, json, pprint, datetime, csv, subprocess, time
 import requests
 
 import pandas as pd
@@ -8,12 +8,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-# TODO refactor jsonl read and save operations
-# make sure to update these paths if the relative path
-# to instruction_following_eval/ changes
+# update these paths if the relative path instruction_following_eval/ changes
 DATA_PATH = os.path.join('.', 'instruction_following_eval', 'data')
-PROMPTS_PATH = os.path.join(
-    DATA_PATH, 'input_data.jsonl')
+PROMPTS_PATH = os.path.join(DATA_PATH, 'input_data.jsonl')
 NUM_PROMPTS = 541
 
 
@@ -24,7 +21,8 @@ def read_jsonl(path: str) -> list:
         return jlines
 
 
-def get_llm_response(messages: list, model: str) -> str:
+def get_llm_response(messages: list, model: str) -> str | None:
+    """Generate a response using the OpenRouter service"""
     OPENROUTER_API_KEY = os.environ['OPENROUTER_API_KEY']
 
     try:
@@ -38,106 +36,79 @@ def get_llm_response(messages: list, model: str) -> str:
                 "messages": messages
             })
         )
-    
         result = response.json()
-        
-    # TODO be more specific with potential exceptions
     except Exception as e:
         print(repr(e))
-        return {'error': 'error'}
-    
-    print(f"Response keys: {result.keys()}")
+        return None
+
+    if 'error' in response:
+        print(response)
+        return None
+
     return result
 
 
-# TODO convert to use input_data.jsonl
 def get_input_data(file: str = PROMPTS_PATH) -> list:
+    """returns input data in batches"""
     input_data = read_jsonl(file)
     return input_data
-
-    # with open(file, mode='r', encoding='utf-8') as jsonl:
-    #     prompts = jsonl.read().splitlines()
-    #     # print(prompts)
-    #
-    #     return_list = []
-    #     for jline in prompts:
-    #         return_dict = json.loads(jline)
-    #         del return_dict['response']
-    #         return_list.append(return_dict)
-    #
-    #     return return_list
     
 
-def get_ifeval_input_data(model: str, prompts: list) -> list:
-    # get llm responses to every prompt
-    prompt_response_list = []
-    for i, prompt in enumerate(prompts):
-        prompt = prompt['prompt']
-        print(f"{i+1}. Prompt: {prompt}")
+def generate_responses(model: str, input_data: list) -> tuple[list, set]:
+    """Generate a response to each prompt in the IfEval benchmark.
+    Return a set of keys for any prompts that didn't get a valid response"""
+    responses = []
+    missing = set()
+    for line in input_data:
+        prompt = line['prompt']
+        print(f"Prompt key: {line['key']}")
         
         messages = [{"role": "user", "content": prompt}]
-        # print(messages)
-
-        response = {'error': 'error'}
+        response = None
         prompt_count = 0
-        while 'error' in response and prompt_count < 5:
-            # keep requesting a response until the rate limit subsides
+        while response is None and prompt_count < 3:
             response = get_llm_response(messages=messages, model=model)
+            if response is None:
+                time.sleep(1)  # the primary source of errors is rate limiting
+            prompt_count += 1
 
-            # to prevent getting stuck if there are many exceptions
-            prompt_count += 1  
-            # print(f"\tResponse: {response}")
+        # if we aren't able to generate a valid response, return its key
+        if response is None:
+            missing.add(line['key'])
+            continue
 
-        prompt_response = {
-            "prompt": prompt,
-            "response": response
-        }
-        prompt_response_list.append(prompt_response)
-    
-    return prompt_response_list
+        responses.append(dict(prompt=prompt, response=response))
+
+    return responses, missing
 
 
-def save_ifeval_input_data(
-        model: str,
-        prompt_responses:
-        list, model_datetime: str,
-        runidx: int=0) -> str:
-    # saved files are differentiated by model and date
-    model_name = model.split('/')[-1]
-    filename = '_'.join([
-        'input_response_data',
-        model_name,
-        model_datetime,
-        str(runidx)]) + '.jsonl'
-    save_filepath = os.path.join(DATA_PATH, filename)
+def save_ifeval_input_data(save_filepath: str,
+                           prompt_responses: list,) -> None:
 
-    with open(save_filepath, mode='w', encoding='utf-8') as save_file:
+    with open(save_filepath, mode='a', encoding='utf-8') as save_file:
         for prompt_response in prompt_responses:
 
-            # reformat the responses to only include content
-            message = prompt_response["response"]["choices"][0]["message"]
-            prompt_response_obj = {
-                "prompt": prompt_response["prompt"],
-                "response": message["content"]
-            }
-            print(prompt_response_obj)
+            try:
+                # reformat the responses to only include content
+                message = prompt_response["response"]["choices"][0]["message"]
+                prompt_response_obj = {
+                    "prompt": prompt_response["prompt"],
+                    "response": message["content"]
+                }
+            # skip responses that don't have the expected data
+            except KeyError as e:
+                print(repr(e))
+                pprint.pprint(prompt_response)
+                continue
             
             save_file.write(json.dumps(prompt_response_obj) + "\n")
-    
-    return save_filepath
 
 
-def get_instruction_ids() -> list:
-    input_data_file = os.path.join(DATA_PATH, 'input_data.jsonl')
-
+def get_instruction_ids(input_data: list) -> list:
     instruction_ids = set()
-    with open(input_data_file, mode='r', encoding='utf-8') as f:
-        jlines = f.read().splitlines()
-        jlines = [json.loads(jline) for jline in jlines]
-        for jline in jlines:
-            instruction_ids |= set(jline["instruction_id_list"])
-    
-    # print(instruction_ids)
+    for line in input_data:
+        instruction_ids |= set(line['instruction_id_list'])
+
     return sorted(instruction_ids)
 
 
@@ -192,11 +163,12 @@ def aggregate_results() -> None:
 #                     df_agg.loc[i, agg_instr] += row[instr]
 
 
-def save_evaluation_results(model: str, model_datetime: str):
+def save_evaluation_results(model: str, model_datetime: str,
+                            input_data: list):
     loose_file = os.path.join(DATA_PATH, 'eval_results_loose.jsonl')
     strict_file = os.path.join(DATA_PATH, 'eval_results_strict.jsonl')
     eval_results_file = os.path.join(DATA_PATH, 'eval_results.csv')
-    all_instruction_ids = get_instruction_ids()
+    all_instruction_ids = get_instruction_ids(input_data)
 
     # pull all true from evaluation categories
     for file, accuracy in [[loose_file, 'loose'], [strict_file, 'strict']]:
@@ -248,39 +220,52 @@ def save_evaluation_results(model: str, model_datetime: str):
             eval_results_writer.writerow(results_row)
 
 
+def build_save_filepath(model: str, model_datetime: str, runIdx: int) -> str:
+    model_name = model.split('/')[-1]
+    filename = '_'.join(['input_response_data', model_name,
+                         model_datetime, str(runIdx)]) + '.jsonl'
+    return os.path.join(DATA_PATH, filename)
+
+
 def main():
     input_data = get_input_data(PROMPTS_PATH)
-    # models = [
-    #     # 'mistralai/mistral-7b-instruct:nitro',
-    #     # 'google/gemma-7b-it:nitro',
-    #     # 'mistralai/mixtral-8x7b-instruct:nitro',
-    #     # 'huggingfaceh4/zephyr-7b-beta',
-    #     'perplexity/sonar-small-chat',
-    #     'perplexity/sonar-small-online'
-    # ]
-    #
-    # for model in models:
-    #     for runidx in range(3):
-    #         print(f"{model} run {runidx + 1}")
-    #
-    #         # separate runs are differentiated by model name and datetime
-    #         model_datetime = datetime.datetime.today().strftime('%F')
-    #
-    #         # generate the responses to feed to the evaluation script
-    #         input_response_data = get_ifeval_input_data(model, prompts)
-    #         # TODO save in batches so you don't waste responses
-    #         saved_file = save_ifeval_input_data(model, input_response_data, model_datetime, runidx)
-    #
-    #         # run the evaluation script
-    #         subprocess.run([
-    #             "python", "-m", "instruction_following_eval.evaluation_main",
-    #             "--input_data=./instruction_following_eval/data/input_data.jsonl",
-    #             "--input_response_data=" + saved_file,
-    #             "--output_dir=./instruction_following_eval/data/"
-    #         ])
-    #
-    #         # save the evaluation results for analysis
-    #         save_evaluation_results(model, model_datetime)
+    missing = {line['key'] for line in input_data}
+
+    models = [
+        # 'mistralai/mistral-7b-instruct:nitro',
+        # 'google/gemma-7b-it:nitro',
+        # 'mistralai/mixtral-8x7b-instruct:nitro',
+        # 'huggingfaceh4/zephyr-7b-beta',
+        'perplexity/sonar-small-chat',
+        'perplexity/sonar-small-online'
+    ]
+
+    for model in models:
+        for runIdx in range(3):
+            print(f"{model} run {runIdx + 1}")
+
+            # differentiate runs by model name and datetime
+            model_datetime = datetime.datetime.today().strftime('%F')
+            save_filepath = build_save_filepath(model, model_datetime, runIdx)
+
+            # generate the responses to feed to the evaluation script
+            model_run_count = 0
+            while len(missing) and model_run_count < 3:
+                # keep generating responses for prompts that didn't get a valid response
+                input_data = [line for line in input_data if line['key'] in missing]
+                input_response_data, missing = generate_responses(model, input_data)
+                save_ifeval_input_data(save_filepath, input_response_data)
+
+            # run the evaluation script
+            subprocess.run([
+                "python", "-m", "instruction_following_eval.evaluation_main",
+                "--input_data=./instruction_following_eval/data/input_data.jsonl",
+                "--input_response_data=" + save_filepath,
+                "--output_dir=./instruction_following_eval/data/"
+            ])
+
+            # save the evaluation results for analysis
+            save_evaluation_results(model, model_datetime, input_data)
 
 
 if __name__ == '__main__':
