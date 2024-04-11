f44e597bcc778674c8bacba26c6842c56b06cb96
Ryan-Saklad <ryansaklad@gmail.com>
2024-04-11 13:18:42 -0400

    created problem types

--- benchmark/constraints/constraint.py
+++ benchmark/constraints/constraint.py
@@ -1,9 +1,12 @@
 from abc import ABC, abstractmethod
+from utils.problem_type import ProblemType
 
 class Constraint(ABC):
     def __init__(self, description: str = "") -> None:
         self.description = description
         self.violations: list[str] = []
+
+        self.problem_type: ProblemType = ProblemType.CONSTRAINT
 
     @abstractmethod
     def validate(self, response: str) -> bool:
---
+++ utils/problem_type.py
@@ -0,0 +1,6 @@
+from enum import Enum, auto
+
+class ProblemType(Enum):
+    ELEMENT_CONSTRAINT = auto()
+    OUTPUT_FORMAT = auto()
+    PROBLEM = auto()
