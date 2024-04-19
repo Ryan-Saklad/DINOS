870efd0d841b4148742e7114bedfd3f6f7577671
Daniel Gwon <dygwon@gmail.com>
2024-04-18 22:24:30 -0400

    Create simple response generation pipeline

--- misc/utils/openrouter_utils.py
+++ misc/utils/openrouter_utils.py
@@ -1,9 +1,11 @@
 import json
 import requests
 import csv
+import time
+import os
 
 
-def generate_response(messages: list, model: str, api_key: str) -> str | None:
+def generate_response(messages: list, model: str, api_key: str) -> dict[str, dict] | None:
     """Generate a response using the OpenRouter service"""
 
     try:
@@ -29,6 +31,29 @@ def generate_response(messages: list, model: str, api_key: str) -> str | None:
     return result
 
 
+def generate_batch_responses(batch_prompts: list, model: str, api_key: str, prompt_limit: int = 3) -> list[list[str]] | None:
+    responses = []
+    for prompt in batch_prompts:
+        messages = [{'role': 'user', 'content': prompt}]
+        response = None
+        prompt_count = 0
+        while response is None and prompt_count < prompt_limit:
+            response = generate_response(messages=messages, model=model, api_key=api_key)
+            if response is None:
+                print(f'Unable to generate response. Attempt {prompt_count+1}')
+                time.sleep(0.5)
+            prompt_count += 1
+
+        if response is None:
+            return None
+
+        response = response['choices'][0]['message']['content']
+        print(response)
+        responses.append([response])
+
+    return responses
+
+
 def read_dinos_csv(file_path: str) -> list[str]:
     contents = list()
     with open(file_path, newline='') as csvfile:
@@ -36,4 +61,21 @@ def read_dinos_csv(file_path: str) -> list[str]:
         for row in csv_reader:
             contents.append(row[0])
 
-    return contents
+    if contents[0] not in ['Prompt', 'Response']:
+        raise csv.Error('Unable to read file')
+
+    return contents[1:]
+
+
+def write_dinos_csv(file_path: str, contents: list[list[str]]) -> None:
+    with open(file_path, 'a+') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter=',')
+        for row in contents:
+            csv_writer.writerow(row)
+
+
+def get_model_name(openrouter_model_name: str) -> str:
+    """Use to strip characters that cause issues with filenames and paths"""
+    model_name = openrouter_model_name.split('/')[-1]
+    model_name = model_name.split(':')[0]
+    return model_name
