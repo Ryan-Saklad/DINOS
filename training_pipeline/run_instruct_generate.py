a47b668b9052fce863c8fbd6b432a5efe36dce4a
Kave Eskandari <39281449+TheSittingCat@users.noreply.github.com>
2024-04-27 21:28:26 -0400

    Add Train Related Files

---
+++ training_pipeline/run_instruct_generate.py
@@ -0,0 +1,4 @@
+import generate_instruct
+generate_instruct.generate_instruct_instances("training_pipeline/training_set/750_Prompts_1.json", "training_pipeline/training_set/750_Responses_1.csv")
+generate_instruct.generate_instruct_instances("training_pipeline/training_set/200_Prompts_2.json", "training_pipeline/training_set/200_Responses_2.csv")
+generate_instruct.generate_instruct_instances("training_pipeline/training_set/50_Prompts_3.json", "training_pipeline/training_set/50_Responses_3.csv")
