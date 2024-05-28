6375b6e6e87b6f9700d1af5681c61681ee5a38cc
Ryan-Saklad <ryansaklad@gmail.com>
2024-05-28 13:09:42 -0400

    Removed extra files and constraints

--- utils/problem_type.py
+++ utils/problem_type.py
@@ -1,9 +1,6 @@
 from enum import Enum, auto
 
 class ProblemType(Enum):
-    ELEMENT_CONSTRAINT = "ELEMENT_CONSTRAINT"
-    OUTPUT_FORMAT = "OUTPUT_FORMAT"
-
     PROBLEM = "PROBLEM"
     MULTIPLE_CHOICE = "MULTIPLE_CHOICE"
     RESPONSE = "RESPONSE"
